b947b10fd01aa9bb94626a80cbfd96826a956cc2
sandorjozsef <sjozsef2000@gmail.com>
2021-10-27 23:56:59 +0200

    maifest + project.toml

--- src/Simulator/runSimulations.jl
+++ src/Simulator/runSimulations.jl
@@ -1,14 +1,3 @@
-#import Pkg
-#Pkg.add("Plots")
-#Pkg.add("OrdinaryDiffEq")
-#Pkg.add("CSV")
-#Pkg.add("DataFrames")
-#Pkg.add("Statistics")
-#Pkg.add("JavaCall")
-#Pkg.add("JLD2")
-#Pkg.add("MAT")
-#Pkg.add("FileIO")  
-#Pkg.add("Pipe")
 
 include("runSimulationOnPatients.jl")
 
