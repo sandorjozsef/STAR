afe1ef2a74f96c6c1c1803199d77435097451fc4
sandorjozsef <sjozsef2000@gmail.com>
2021-11-10 22:42:12 +0100

    doksi

--- src/Simulator/runSimulations.jl
+++ src/Simulator/runSimulations.jl
@@ -1,6 +1,6 @@
 include("runSimulationOnPatients.jl")
 
-srcDir = pwd() * "/patients_data/original/HU_mat"
+srcDir = pwd() * "/patients_data/original/interesting_patients_mat"
 
 dstDir = pwd() * "/patients_data/simulated/julia_results"
 
@@ -13,16 +13,16 @@ simulation = Simulation();
 # 1 -> STAR recommended
 # 2 -> SIMPLE
 # 3 -> HISTORIC
-simulation.mode = 3 ; 
+simulation.mode = 1 ; 
 
 # Only for STAR and SIMPLE
 # longest allowed treatment: 1 / 2 / 3
-simulation.longest_allowed = 3;
+simulation.longest_allowed = 3 ;
 
 # Only for STAR and SIMPLE (HISTORIC is always historic)
 # 1 -> exact longest allowed
 # 2 -> historic
-simulation.protocol_timing = 1 ;
+simulation.protocol_timing = 2 ;
 
 # Only for SIMPLE controller
 # 1 -> low nutrition 
