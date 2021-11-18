2cb4e59fa29d648232be9441777b88af7e5463c7
sandorjozsef <sjozsef2000@gmail.com>
2021-11-18 19:32:03 +0100

    doksi konzi

--- src/GUI/example2.jl
+++
@@ -1,8 +0,0 @@
-function run(asd)
-    printl(asd)
-end
-
- #ThreadPools.spawnbg(runSimulationsGUI(simInfo))
-    #@async checked_fetch(spawn_backGround(runSimulationsGUI(simInfo)))
-    #Threads.@threadcall runSimulationsGUI(siminfo)
-    #checked_fetch(spawnbg(runSimulations))
